32d3dc0f2ecaac4743545c611dad193fe1cc5081
Nick McVeity <nmcveity@gmail.com>
2013-10-05 20:43:19 -0700

    Fixing tabs/spaces.
    (Must stop sublime from doing this!)

--- luajitrays/main.lua
+++ luajitrays/main.lua
@@ -38,10 +38,10 @@ local fixed_art = {
 }
 
 -- Uncomment for Lua logo :)
--- art = fixed_art
+art = fixed_art
 
 local function object(x, y)
-	return {k=x, j=y}
+  return {k=x, j=y}
 end
 
 ffi_cdef [[
@@ -87,11 +87,11 @@ local function F()
   local nr = #art
   local nc = #art[1]
   for k = nc,1,-1 do
-  	for j = nr,1,-1 do
-  		if art[j]:sub(nc-k+1, nc-k+1) ~= ' ' then
-  			objects[#objects+1] = object(-k+1, -(nr - j))
-  		end
-  	end
+    for j = nr,1,-1 do
+      if art[j]:sub(nc-k+1, nc-k+1) ~= ' ' then
+        objects[#objects+1] = object(-k+1, -(nr - j))
+      end
+    end
   end
 end
 
@@ -113,28 +113,28 @@ local function T(o, d)
   local m = 0
   local p = -o.z / d.z
   if 0.01 < p then
-  	t = p
-  	vset(n, 0, 0, 1)
-  	m = 1
+    t = p
+    vset(n, 0, 0, 1)
+    m = 1
   end
   o = vadd(o, vector(0,3,-4))
-	for index=1,#objects do -- obj in ipairs(objects) do
-		--local p = vadd(o, vector(obj.k, 0, obj.j))
-		local p = vector(o.x + objects[index].k, o.y, o.z + objects[index].j)
+  for index=1,#objects do -- obj in ipairs(objects) do
+    --local p = vadd(o, vector(obj.k, 0, obj.j))
+    local p = vector(o.x + objects[index].k, o.y, o.z + objects[index].j)
     local b = vdot(p,d)
-		local c = vsqr(p) - 1
-		local b2 = b * b
-		-- Does the ray hit the sphere?
-		if b2 > c then
-			local q = b2 - c
-			local s = -b - math_sqrt(q)
-			if s < t and s > 0.01 then
-				t = s
-				n = vnormalize(vadd(p, vscale(d, t)))
-			  m = 2
-			end
-		end	 
-	end
+    local c = vsqr(p) - 1
+    local b2 = b * b
+    -- Does the ray hit the sphere?
+    if b2 > c then
+      local q = b2 - c
+      local s = -b - math_sqrt(q)
+      if s < t and s > 0.01 then
+        t = s
+        n = vnormalize(vadd(p, vscale(d, t)))
+        m = 2
+      end
+    end   
+  end
   return m, t, n
 end
 
@@ -147,32 +147,32 @@ local TILES = {
 }
 
 local function S(o, d)
-	local m, t, n = T(o, d)
-	if m == 0 then
-		-- No sphere found and the ray goes upwards: generate a sky color
-		local p = 1 - d.z
-		p = p * p
-		p = p * p
-		return vector(p * 0.7, p * 0.6, p)
-	end
-	-- A sphere was maybe hit
-	local h = vadd(o, vscale(d, t))
-	local l = vnormalize(vadd(vector(9+R(), 9+R(), 16), vscale(h,-1)))
+  local m, t, n = T(o, d)
+  if m == 0 then
+    -- No sphere found and the ray goes upwards: generate a sky color
+    local p = 1 - d.z
+    p = p * p
+    p = p * p
+    return vector(p * 0.7, p * 0.6, p)
+  end
+  -- A sphere was maybe hit
+  local h = vadd(o, vscale(d, t))
+  local l = vnormalize(vadd(vector(9+R(), 9+R(), 16), vscale(h,-1)))
   -- Calculate lambertian factor
   local b = vdot(l,n)
   -- Calculate illumination factor
   if b < 0 or T(h, l) ~= 0 then
-  	b = 0
+    b = 0
   end
   if m == 1 then
-  	local h2 = vscale(h,0.2)
+    local h2 = vscale(h,0.2)
     return vscale(TILES[band((math_ceil(h2.x) + math_ceil(h2.y)),1)+1], b * 0.2 + 0.1)
   end
   local r = vadd(d, vscale(n, vdot(n, vscale(d, -2))))
   -- Calculate the color 'p' with diffuse and specular component
   local s = 0
   if b > 0 then
-  	s = 1
+    s = 1
   end
   -- Note to self: removing branch above (code below) did not improve times
   --local s = math_max(0, math_min(1, math_ceil(b)))
@@ -193,10 +193,10 @@ local w, h = 512, 512
 local argv = arg
 
 if #argv > 0 then
-	w = tonumber(argv[1])
+  w = tonumber(argv[1])
 end
 if #argv > 1 then
-	h = tonumber(argv[2])
+  h = tonumber(argv[2])
 end
 
 local g = vnormalize(vector(-5.5, -16, 0))
@@ -215,21 +215,21 @@ local i = 0
 local CAM_POS = vector(17,16,8)
 
 for y=h-1,0,-1 do
-	for x=w-1,0,-1 do
-		local p = vector(13,13,13)
-		-- Cast 64 rays per pixel
-		for r=1,RAYS do
-			local t = vadd(vscale(vscale(a, R()-0.5), 99), vscale(vscale(b,R()-0.5), 99))
-			local ra = vscale(a, R()+x)
-			local rb = vscale(b, R()+y)
-			local d = vnormalize(vadd(vscale(t, -1), vscale(vadd(vadd(ra, rb), c), 16)))
-			p = vadd(vscale(S(vadd(CAM_POS, t), d), 3.5*SCALE), p)
-		end
+  for x=w-1,0,-1 do
+    local p = vector(13,13,13)
+    -- Cast 64 rays per pixel
+    for r=1,RAYS do
+      local t = vadd(vscale(vscale(a, R()-0.5), 99), vscale(vscale(b,R()-0.5), 99))
+      local ra = vscale(a, R()+x)
+      local rb = vscale(b, R()+y)
+      local d = vnormalize(vadd(vscale(t, -1), vscale(vadd(vadd(ra, rb), c), 16)))
+      p = vadd(vscale(S(vadd(CAM_POS, t), d), 3.5*SCALE), p)
+    end
     out[i] = p.x
     out[i+1] = p.y
     out[i+2] = p.z
     i = i + 3
-	end
+  end
 end
 
 local header = string_format("P6 %d %d 255 ", w, h)
--- luarays/main.lua
+++ luarays/main.lua
@@ -19,12 +19,12 @@ local art = {
 }
 
 local function object(x, y)
-	return {k=x, j=y}
+  return {k=x, j=y}
 end
 
 local vector
 local vector_meta = {
-	__index = {
+  __index = {
     dot = function (self, v2)
       return self.x * v2.x + self.y * v2.y + self.z * v2.z
     end,
@@ -35,21 +35,21 @@ local vector_meta = {
       return vector(self.x * t, self.y * t, self.z * t)
     end,
     cross = function (self, v2)
-    	return vector(self.y*v2.z-self.z*v2.y,self.z*v2.x-self.x*v2.z,self.x*v2.y-self.y*v2.x)
-  	end,
+      return vector(self.y*v2.z-self.z*v2.y,self.z*v2.x-self.x*v2.z,self.x*v2.y-self.y*v2.x)
+    end,
     add = function (self, v2)
-    	return vector(self.x+v2.x, self.y+v2.y, self.z+v2.z)
-  	end,
-  	set = function (self, x, y, z)
-  	  self.x, self.y, self.z = x, y, z
-  	end
+      return vector(self.x+v2.x, self.y+v2.y, self.z+v2.z)
+    end,
+    set = function (self, x, y, z)
+      self.x, self.y, self.z = x, y, z
+    end
   }
 }
 
 function vector(x, y, z)
-	local v = {x = x or 0, y = y or 0, z = z or 0}
-	setmetatable(v, vector_meta)
-	return v
+  local v = {x = x or 0, y = y or 0, z = z or 0}
+  setmetatable(v, vector_meta)
+  return v
 end
 
 local objects = {}
@@ -58,16 +58,16 @@ local function F()
   local nr = #art
   local nc = #art[1]
   for k = nc,1,-1 do
-  	for j = nr,1,-1 do
-  		if art[j]:sub(nc-k+1, nc-k+1) ~= ' ' then
-  			objects[#objects+1] = object(-k+1, -(nr - j))
-  		end
-  	end
+    for j = nr,1,-1 do
+      if art[j]:sub(nc-k+1, nc-k+1) ~= ' ' then
+        objects[#objects+1] = object(-k+1, -(nr - j))
+      end
+    end
   end
 end
 
 local function R()
-	return math_random()
+  return math_random()
 end
 
 --The intersection test for line [o,v].
@@ -81,62 +81,62 @@ local function T(o, d)
   local m = 0
   local p = -o.z / d.z
   if 0.01 < p then
-  	t = p
-  	n:set(0, 0, 1)
-  	m = 1
+    t = p
+    n:set(0, 0, 1)
+    m = 1
   end
   o = o:add(vector(0,3,-4))
-	for index, obj in ipairs(objects) do
-		local p = o:add(vector(obj.k, 0, obj.j))
-		local b = p:dot(d)
-		local c = p:dot(p) - 1
-		local b2 = b * b
-		-- Does the ray hit the sphere?
-		if b2 > c then
-			local q = b2 - c
-			local s = -b - math_sqrt(q)
-			if s < t and s > 0.01 then
-				t = s
-				n = p:add(d:scale(t)):normalize()
-			  m = 2
-			end
-		end	 
-	end
+  for index, obj in ipairs(objects) do
+    local p = o:add(vector(obj.k, 0, obj.j))
+    local b = p:dot(d)
+    local c = p:dot(p) - 1
+    local b2 = b * b
+    -- Does the ray hit the sphere?
+    if b2 > c then
+      local q = b2 - c
+      local s = -b - math_sqrt(q)
+      if s < t and s > 0.01 then
+        t = s
+        n = p:add(d:scale(t)):normalize()
+        m = 2
+      end
+    end   
+  end
   return m, t, n
 end
 
 local function S(o, d)
-	local m, t, n  = T(o, d)
-	local on = vector(n.x, n.y, n.z)
-	if m == 0 then
-		-- No sphere found and the ray goes upwards: generate a sky color
-		local p = 1 - d.z
-		p = p * p
-		p = p * p
-		return vector(p * 0.7, p * 0.6, p)
-	end
-	-- A sphere was maybe hit
-	local h = o:add(d:scale(t))
-	local l = vector(9+R(), 9+R(), 16):add(h:scale(-1)):normalize()
+  local m, t, n  = T(o, d)
+  local on = vector(n.x, n.y, n.z)
+  if m == 0 then
+    -- No sphere found and the ray goes upwards: generate a sky color
+    local p = 1 - d.z
+    p = p * p
+    p = p * p
+    return vector(p * 0.7, p * 0.6, p)
+  end
+  -- A sphere was maybe hit
+  local h = o:add(d:scale(t))
+  local l = vector(9+R(), 9+R(), 16):add(h:scale(-1)):normalize()
   -- Calculate lambertian factor
   local b = l:dot(n)
   -- Calculate illumination factor
   if b < 0 or T(h, l, t, n) ~= 0 then
-  	b = 0
+    b = 0
   end
   if m == 1 then
-  	h = h:scale(0.2)
-  	if ((math_ceil(h.x) + math_ceil(h.y)) % 2) == 1 then
-  		return vector(3,1,1):scale(b * 0.2 + 0.1)
-  	else
-  		return vector(3,3,3):scale(b * 0.2 + 0.1)
-  	end
+    h = h:scale(0.2)
+    if ((math_ceil(h.x) + math_ceil(h.y)) % 2) == 1 then
+      return vector(3,1,1):scale(b * 0.2 + 0.1)
+    else
+      return vector(3,3,3):scale(b * 0.2 + 0.1)
+    end
   end
   local r = d:add(on:scale(on:dot(d:scale(-2))))
   -- Calculate the color 'p' with diffuse and specular component
   local s = 0
   if b > 0 then
-  	s = 1
+    s = 1
   end
   local p = l:dot(r:scale(s))
   local p33 = p * p
@@ -155,10 +155,10 @@ local w, h = 512, 512
 local argv = arg
 
 if #argv > 0 then
-	w = tonumber(argv[1])
+  w = tonumber(argv[1])
 end
 if #argv > 1 then
-	h = tonumber(argv[2])
+  h = tonumber(argv[2])
 end
 
 io.write(string.format("P6 %d %d 255 ", w, h))
@@ -179,32 +179,32 @@ local offset = 1
 local jump = 1
 
 local function clamp(x)
-	if x < 1 then
-		return 1
-	elseif x > 255 then
-		return 255
-	else
-		return math_floor(x)
-	end
+  if x < 1 then
+    return 1
+  elseif x > 255 then
+    return 255
+  else
+    return math_floor(x)
+  end
 end
 
 local RAYS = 64
 local SCALE = 64 / RAYS
 
 for y=h-1,0,-1 do
-	for x=w-1,0,-1 do
-		local p = vector(13,13,13)
-		-- Cast 64 rays per pixel
+  for x=w-1,0,-1 do
+    local p = vector(13,13,13)
+    -- Cast 64 rays per pixel
 
-		for r=1,RAYS do
-			local t = a:scale(R()-0.5):scale(99):add(b:scale(R()-0.5):scale(99))
-			local ra = a:scale(R()+x)
-			local rb = b:scale(R()+y)
-			local d = t:scale(-1):add(ra:add(rb):add(c):scale(16)):normalize()
-			p = S(vector(17,16,8):add(t), d):scale(3.5*SCALE):add(p)
-		end
-		io_write(string_char(clamp(p.x)), 
-			string_char(clamp(p.y)),
-			string_char(clamp(p.z)))
-	end
+    for r=1,RAYS do
+      local t = a:scale(R()-0.5):scale(99):add(b:scale(R()-0.5):scale(99))
+      local ra = a:scale(R()+x)
+      local rb = b:scale(R()+y)
+      local d = t:scale(-1):add(ra:add(rb):add(c):scale(16)):normalize()
+      p = S(vector(17,16,8):add(t), d):scale(3.5*SCALE):add(p)
+    end
+    io_write(string_char(clamp(p.x)), 
+      string_char(clamp(p.y)),
+      string_char(clamp(p.z)))
+  end
 end
